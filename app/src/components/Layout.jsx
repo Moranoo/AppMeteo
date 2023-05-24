6fe5236a3c8aedce282e19dcd56ef03ff36f47d2
LAPTOP-MH6K4TCI\12mou <12mourad4gmail.com>
2023-05-24 16:15:42 +0200

    header

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState, createContext } from "react";
+import { Toggle, ToggleItem } from "@tremor/react";
+import { TbTemperatureCelsius, TbTemperatureFahrenheit } from "react-icons/tb";
 
 export const DegreeContext = createContext();
 
@@ -16,7 +18,17 @@ export const Layout = ({ children }) => {
           width: "100%",
         }}
       >
-        <header className="text-3xl"></header>
+        <header className="text-3xl">
+          <Toggle
+            color="zinc"
+            defaultValue="1"
+            onValueChange={(value) => console.log(value)}
+          >
+            <ToggleItem value="1" icon={TbTemperatureCelsius} />
+            <ToggleItem value="2" icon={TbTemperatureFahrenheit} />
+          </Toggle>
+
+        </header>
         <main>{children}</main>
         <footer></footer>
       </div>
